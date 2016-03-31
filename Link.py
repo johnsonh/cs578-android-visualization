40dcbef778721574c2a154c207719aace55b137e
Hsieh <hsiehj@amazon.com>
2016-03-31 01:54:05 -0700

    adding d3 stuff, moved folder structure

--- Link.py
+++ Link.py
@@ -6,7 +6,7 @@ class Link(CommonEqualityMixin):
 		self.target = componentNameToIdxMap[intentDict["component"]]
 
 		self.consumerMethod = intentDict["consumerMethod"]
-		self.action = intentDict["action"]
+		self.action = intentDict["action"] # TODO: turn this into numeric value for different weighted lines?
 
 	def reprJSON(self):	
 		return dict(source=self.source, target=self.target, consumerMethod=self.consumerMethod, action=self.action) 
