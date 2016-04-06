7a7ddec707c139907c2c89953c3542f0a70d866a
Hsieh <hsiehj@amazon.com>
2016-04-05 22:19:44 -0700

    trying flat, trying to group nodes

--- d3/d3.js
+++ d3/d3.js
@@ -1,5 +1,5 @@
-var width = 960,
-    height = 500;
+var width = 1000,
+    height = 1000;
 
 var color = d3.scale.category20();
 
@@ -18,6 +18,27 @@ d3.json("https://raw.githubusercontent.com/johnsonh/cs578-android-visualization/
   force
       .nodes(graph.nodes)
       .links(graph.links)
+      .size([width, height])
+      .linkDistance(function(l, i) {
+          var n1 = l.source, n2 = l.target;
+          var strength = 150;
+          if (n1.parentApp == n2.parentApp) {
+            return strength / 5;
+          }
+          return strength;
+        })
+      .linkStrength(function(l, i) {
+          var n1 = l.source, n2 = l.target;
+          var strength = 10;
+          if (n1.parentApp == n2.parentApp) {
+            return strength * 2;
+          }
+          return 10;
+        })
+      .gravity(0.1)   // gravity+charge tweaked to ensure good 'grouped' view (e.g. green group not smack between blue&orange, ...
+      .charge(-100)    // ... charge is important to turn single-linked groups to the outside
+      .friction(0.5)   // friction adjusted to get dampened display: less bouncy bouncy ball [Swedish Chef, anyone?]
+
       .start();
 
   var link = svg.selectAll(".link")
@@ -28,13 +49,13 @@ d3.json("https://raw.githubusercontent.com/johnsonh/cs578-android-visualization/
 
   var node = svg.selectAll(".node")
       .data(graph.nodes)
-    .enter().append("circle")
+      .enter().append("circle")
       .attr("class", "node")
       .attr("r", 5)
       .style("fill", function(d) { return color(d.parentApp); })
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout)
       .call(force.drag);
-
-  node.call(force.drag); 
 
   node.append("title")
       .text(function(d) { return d.name; });
@@ -53,4 +74,17 @@ d3.json("https://raw.githubusercontent.com/johnsonh/cs578-android-visualization/
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; });
   });
+
+  function mouseover() {
+    d3.select(this).select("circle").transition()
+        .duration(750)
+        .attr("r", 160);
+  }
+
+  function mouseout() {
+    d3.select(this).select("circle").transition()
+        .duration(750)
+        .attr("r", 8);
+  }
 });
+
